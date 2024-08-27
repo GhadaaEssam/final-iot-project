297396a1c4c4fe3f5dc19c91a589a42910350314
GhadaaEssam <167557174+GhadaaEssam@users.noreply.github.com>
2024-08-27 21:33:26 +0300

    Update Door.dart

--- flutter-code/lib/Door.dart
+++ flutter-code/lib/Door.dart
@@ -7,28 +7,43 @@ class DoorPage extends StatefulWidget {
 }
 
 class _DoorPageState extends State<DoorPage> {
+  //controller for the topic input field
   final topicController = TextEditingController();
+
+  //list to store received messages
   final List<String> _messages = [];
   final List<ChartData> _chartData = [];
+
   final MQTTClientWrapper mqttClientWrapper = MQTTClientWrapper();
+
+  //variable to track the current subscribed topic
   String? _currentTopic;
+
+  //variable to store the current reading received from MQTT
   double _currentReading = 0.0;
-  bool _isOn = false; // Track the button state
+
+  //boolean to track the switch/button state
+  bool _isOn = false;
 
   @override
   void initState() {
     super.initState();
     mqttClientWrapper.prepareMqttClient();
+    
     mqttClientWrapper.onMessageReceived = (message) {
       setState(() {
         _messages.add(message);
+
         try {
           _currentReading = double.parse(message);
           _chartData.add(ChartData(DateTime.now(), _currentReading));
+
+          //maintain a maximum of 20 data points
           if (_chartData.length > 20) {
             _chartData.removeAt(0);
           }
         } catch (e) {
+          //handle any errors
           print('Error parsing message to double: $e');
         }
       });
@@ -36,18 +51,23 @@ class _DoorPageState extends State<DoorPage> {
     };
   }
 
+  //subscribe to the specified topic
   void _subscribeToTopic(String topic) {
-    topic = "door/control";
+    topic = "door/control"; //topic for door control
+
+    //only subscribe if the topic is valid and different from the current one
     if (topic.isNotEmpty && topic != _currentTopic) {
       mqttClientWrapper.subscribeToTopic(topic);
       _currentTopic = topic;
     }
   }
 
+  //function to toggle the switch and publish corresponding MQTT messages
   void _toggleSwitch(bool value) {
     setState(() {
-      _isOn = value;
-      // Publish message based on switch state
+      _isOn = value; 
+
+      //publish message based on switch state (OPEN or CLOSE)
       final message = _isOn ? 'OPEN' : 'CLOSE';
       mqttClientWrapper.publishMessage("door/control", message);
       print('Switch is ${_isOn ? "On" : "Off"} - Published: $message');
@@ -61,7 +81,7 @@ class _DoorPageState extends State<DoorPage> {
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [
-              Color.fromARGB(255, 6, 5, 49),
+              Color.fromARGB(255, 6, 5, 49), 
               Color(0xFFdbc6b0),
             ],
             begin: Alignment.topLeft,
@@ -70,29 +90,29 @@ class _DoorPageState extends State<DoorPage> {
         ),
         child: Center(
           child: Stack(
-            alignment: Alignment.center,
+            alignment: Alignment.center, 
             children: [
               Positioned(
                 top: 0,
                 child: Image.asset(
-                  _isOn ? 'assets/opend.png' : 'assets/closed.png',
-                  width: 100,  // Set the width
-                  height: 500, // Set the height
+                  _isOn ? 'assets/opend.png' : 'assets/closed.png', //different images based on door state
+                  width: 100, 
+                  height: 500, 
                 ),
               ),
               Align(
                 alignment: Alignment.bottomCenter,
                 child: Padding(
-                  padding: const EdgeInsets.only(bottom: 300.0), // Adjust the bottom padding to move the switch up or down
+                  padding: const EdgeInsets.only(bottom: 300.0),
                   child: Transform.scale(
-                    scale: 1.5, // Adjust the scale factor to make the switch larger
+                    scale: 1.5,
                     child: Switch(
-                      value: _isOn,
-                      onChanged: _toggleSwitch,
-                      activeColor: Color.fromARGB(255, 155, 139, 122),
+                      value: _isOn, 
+                      onChanged: _toggleSwitch, 
+                      activeColor: Color.fromARGB(255, 155, 139, 122), 
                       inactiveThumbColor: Color.fromARGB(255, 6, 5, 49),
-                      inactiveTrackColor:Color(0xFF4d6489),
-                      activeTrackColor: Color(0xFFdbc6b0),
+                      inactiveTrackColor: Color(0xFF4d6489),
+                      activeTrackColor: Color(0xFFdbc6b0), 
                     ),
                   ),
                 ),
